fc6eb019a30c883edf1e8b273e492e52402494b7
Igor Camargo <igorfelcam@hotmail.com>
2016-04-06 15:09:06 -0300

    iframe sem scroll

--- js/ataque.js
+++ js/ataque.js
@@ -1,8 +1,40 @@
+
+var req;
+
 function coordenadas(event) {
 	x=event.clientX;
 	y=event.clientY;
 	// alert("X coords: " + x + ", Y coords: " + y);
 	alert("X coords: " + x);
+// ==========================================================================================
+// Verificando Browser
+	if(window.XMLHttpRequest) {
+	   req = new XMLHttpRequest();
+	}
+	else if(window.ActiveXObject) {
+	   req = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	var url = "192.168.1.158:9600/?"+x;					// comando arduino - <a href=\"/?cen\"\">Centralizar câmera</a>
+// Chamada do método open para processar a requisição
+	req.open("Get", url, true);
+// Quando o objeto recebe o retorno, chamamos a seguinte função;
+	req.onreadystatechange = function() {
+// Exibe a mensagem enquanto carrega
+		if(req.readyState == 1) {
+			document.getElementById('retorno').innerHTML = 'enviando... '+x;
+			// alert("enviando "+x);
+		}
+// Verifica se o Ajax realizou todas as operações corretamente
+		if(req.readyState == 4 && req.status == 200) {
+// Resposta retornada pelo busca.php
+			// alert("enviado "+x);
+			// var resposta = req.responseText;
+// Abaixo colocamos a(s) resposta(s) na div resultado
+			document.getElementById('retorno').innerHTML = 'enviado '+x;
+		}
+	}
+	req.send(null);
+// ==========================================================================================
 }
 
 function mouse(e) {
